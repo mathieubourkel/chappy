4dc422f91bdc124b23409e1ec335bbe551057196
LaigleJ <you@example.com>
2023-11-22 17:31:13 +0100

    signup in progress add button radio

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,18 +1,9 @@
-import * as React from "react";
 import FormUser from "../../components/Authentication/formUser";
 import { NavbarVisitor } from "../../components/Navbar/NavbarVisitor";
-import { Checkbox, Typography } from "@material-tailwind/react";
+import { Radio, Typography } from "@material-tailwind/react";
 
 export default function Signup(props: any) {
   const { handleSubmitUser } = props;
-  const [checked, setChecked] = React.useState(false);
-  const handleChangeCompagny = () => {
-    setChecked(!checked)
-  }
-  const [check, setCheck] = React.useState(false);
-  const handleChangeEmployee = () => {
-    setCheck(!check)
-  }
   return (
     <>
       <header>
@@ -22,12 +13,30 @@ export default function Signup(props: any) {
         <h1>Créer mon compte</h1>
         <FormUser handleSubmitUser={handleSubmitUser} />
 
-        <article className="w-30 flex justify-center">
-        <form className="flex justify-center flex-col">
-        <Checkbox label={<Typography color="blue-gray" className="flex font-medium">Ajouter mon entreprise</Typography>} name="checkCompagny" id="checkCompagny" crossOrigin={undefined} checked={checked} onChange={handleChangeCompagny} /> 
-         <Checkbox label={<Typography color="blue-gray" className="flex font-medium">Je suis salarié d'une entreprise enregistrée</Typography>} name="checkEmployee"  id="chekEmployee" crossOrigin={undefined} checked={check} onChange={handleChangeEmployee} /> 
-        </form>
-        </article>
+        <div className="w-30 flex justify-center">
+          <div className="flex justify-center flex-col">
+            <Radio
+              label={
+                <Typography color="blue-gray" className="flex font-medium">
+                  Ajouter mon entreprise
+                </Typography>
+              }
+              name="checkCompagny"
+              ripple={true}
+              crossOrigin={undefined}
+            />
+            <Radio
+              label={
+                <Typography color="blue-gray" className="flex font-medium">
+                  Je suis salarié d'une entreprise enregistrée
+                </Typography>
+              }
+              name="checkEmployee"
+              ripple={false}
+              crossOrigin={undefined}
+            />
+          </div>
+        </div>
       </main>
     </>
   );
