aa3c38df9480de3244feea92b29af669d31eac20
Laetitia Ashry <ashry.laetitia@gmail.com>
2024-01-18 12:17:17 +0100

    sidebar fix ENCORE

--- src/components/Layers/Sidebar/NestedMenuSidebar.tsx
+++ src/components/Layers/Sidebar/NestedMenuSidebar.tsx
@@ -1,20 +1,11 @@
-import {
-    AccordionBody,
-} from "@material-tailwind/react";
 import {NavLink} from "react-router-dom";
-import {
-    FontAwesomeIcon
-} from "@fortawesome/react-fontawesome";
-import {
-    faChartPie
-} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChartPie } from "@fortawesome/free-solid-svg-icons";
 
 export default function NestedMenuSidebar(props:any) {
-
     const { steps } = props;
-
     return (
-            <AccordionBody className="py-1 max-h-[20vh] custom-scroll">
+        <>
                     {steps && steps.map((step:any) => (
                         <div  className={"py-1.5 px-3 text-light-200 text-sm sidebar-list"} key={step.id}>
                             <NavLink to={'/project/step'} className={"flex items-center"}>
@@ -28,7 +19,7 @@ export default function NestedMenuSidebar(props:any) {
                         </div>
 
                     ))}
-            </AccordionBody>
+        </>
     );
 
 }
