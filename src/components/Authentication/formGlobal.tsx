20602f819f5cde3d5f909a45a97f432d5d705f32
LaigleJ <you@example.com>
2024-01-24 11:02:59 +0100

    hello

--- src/components/Authentication/formGlobal.tsx
+++ src/components/Authentication/formGlobal.tsx
@@ -11,6 +11,7 @@ import {
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 
 
